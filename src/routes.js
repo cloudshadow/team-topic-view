6897f3f894c0b7dc807dfa226d204ab049cbedbd
CloudShadow <cloudshadow.hjy@gmail.com>
2018-01-15 15:32:41 +0800

    remove check login function

--- src/routes.js
+++ src/routes.js
@@ -4,20 +4,6 @@ import App from './containers/App';
 import LoginPage from './containers/LoginPage'; // eslint-disable-line import/no-named-as-default
 import HomePage from './containers/HomePage'; // eslint-disable-line import/no-named-as-default
 
-const checkLogin = (store) => {
-  return (nextState, replace) => {
-    const user = store.getState().authState.user;
-    console.log(user)
-    console.log(localStorage.user)
-    if (user || localStorage.user) {
-      console.log('in')
-      // replace({
-      //   pathname: '/home'
-      // });
-    }
-  };
-};
-
 const requireAuth = (store) => {  
   return (nextState, replace) => {
     const user = store.getState().authState.user;
@@ -34,7 +20,7 @@ const requireAuth = (store) => {
 export const routes = (store) => {
   return (
     <Route path="/" component={App}>
-      <IndexRoute component={LoginPage} onEnter={checkLogin(store)}/>
+      <IndexRoute component={LoginPage}/>
       <Route path="home" component={HomePage} onEnter={requireAuth(store)}/>
     </Route>
   );
